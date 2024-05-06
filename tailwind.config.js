b62bb9013a01076db5ab8b390184d6be926a6816
Barış Hakverdi <128982458+barishakverdi@users.noreply.github.com>
2024-05-06 11:27:17 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './src/views/**/*.html',
+        './src/js/**/*.js',
+        './index.html',
+    ],
+    theme: {
+        extend: {
+            screens: {
+                'min-xxl': {'min': '1441px'},
+                'min-xl': {'min': '1281px'},
+                'min-lg': {'min': '1025px'},
+                'min-md': {'min': '769px'},
+                'min-sm': {'min': '577px'},
+                'xxl': {'max': '1440px'},
+                'xl': {'max': '1280px'},
+                'lg': {'max': '1024px'},
+                'md': {'max': '768px'},
+                'sm': {'max': '576px'},
+            },
+
+            colors: {
+                primary: '#137ff2',
+                dark: '#091929',
+                light: '#dcdfe3',
+                text: '#545557',
+                white: '#FFFFFF',
+                black: '#000000',
+            },
+
+            zIndex: {
+                1: '1',
+                2: '2',
+                3: '3',
+                4: '4',
+            },
+
+            flexGrow: {
+                1: '1',
+            },
+        },
+    },
+    plugins: [
+        // require('@tailwindcss/container-queries'),
+        // require('prettier-plugin-tailwindcss'),
+    ],
+};
+
---
+++ vite.config.js
@@ -0,0 +1,36 @@
+// vite.config.js
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import handlebars from 'vite-plugin-handlebars';
+
+// @see https://vitejs.dev/config/
+
+export default defineConfig({
+    build: {
+        minify: true,
+        outDir: "../dist/",
+        cssCodeSplit: false,
+        manifest: true,
+
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                nested: resolve(__dirname, 'src/views/index.html'),
+                aboutUs: resolve(__dirname, 'src/views/about-us.html'),
+                contact: resolve(__dirname, 'src/views/contact.html'),
+            },
+        },
+    },
+
+    root: "src/",
+
+    css: {
+        devSourcemap: true,
+    },
+
+    plugins: [
+        handlebars({
+            partialDirectory: resolve(__dirname, 'src/views/block'),
+        }),
+    ],
+})
